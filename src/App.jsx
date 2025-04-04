d8b4df74c0d57f5d9ef6e79c37e4b1c6c7866138
alexciccarese <alex.ciccarese@gmail.com>
2025-04-04 17:41:15 +0200

    Implement movie search functionality with API integration and display results

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
-import { useState } from "react";
-import MovieContext from "./context/MovieContext";
-import { BrowserRouter, Routes, Route } from "react-router"
+import { useEffect, useState } from "react";
+
+const api_key = import.meta.env.MOVIE_DB_API_KEY;
 
 export default function App() {
+  const base_movies_api_url = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${search}`;
 
   const [inputValue, setInputValue] = useState('')
   const [search, setSearch] = useState('')
+  const [filteredMovies, setFilteredMovies] = useState(base_movies_api_url)
+
+  useEffect(() => {
+
+    setFilteredMovies(movies.filter(movie => movie.title.toLowerCase().includes(inputValue.toLocaleLowerCase())))
+  },[])
 
   function handleFormSubmit(e) {
     e.preventDefault(e)
     setSearch(inputValue)
     console.log(inputValue);
+
+
+    fetch(base_movies_api_url)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      
+    })
   }
 
 
@@ -35,6 +50,23 @@ export default function App() {
         <button type="submit" className="btn btn-primary mt-2" onClick={handleFormSubmit}>
           Cerca
         </button>
+      </div>
+
+
+      <div>
+        <div className="container mb-5">
+          <div className="row gap-2">
+            {filteredMovies.map(movie => {
+
+              <div key={movie.id} className="card">
+                <h4>{movie.title}</h4>
+                <h5>{movie.original_title}</h5>
+                <p>{movie.original_language}</p>
+                <p>{movie.vote_count}</p>
+              </div>
+            })}
+          </div>
+        </div>
       </div>
 
     </>
