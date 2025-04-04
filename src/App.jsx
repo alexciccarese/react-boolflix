2ef9a7ad95d351b49b48ea91247ce5a29529bd21
alexciccarese <alex.ciccarese@gmail.com>
2025-04-04 17:20:44 +0200

    Implement search functionality in App component with input handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,41 @@
 import { useState } from "react";
+import MovieContext from "./context/MovieContext";
 import { BrowserRouter, Routes, Route } from "react-router"
 
 export default function App() {
+
+  const [inputValue, setInputValue] = useState('')
+  const [search, setSearch] = useState('')
+
+  function handleFormSubmit(e) {
+    e.preventDefault(e)
+    setSearch(inputValue)
+    console.log(inputValue);
+  }
 
 
   return (
 
     <>
-    <div className="container">
-      <h1>Boolflix</h1>
+      <div className="container">
+        <h1>Boolflix</h1>
 
-      
-      <label className="form-label">Cerca per titolo</label>
-      <input
-        type="text"
-        name="search-movie"
-        id="search-movie"
-        className="form-control"
-        aria-describedby="movie-helper"
-        placeholder='Cerca titolo qui'
-      />
 
-      <button 
-      type="submit" 
-      className="btn btn-primary mt-2"
-      >
-        Cerca
-      </button>
-    </div>
+        <label className="form-label">Cerca per titolo</label>
+        <input
+          type="text"
+          name="search-movie"
+          id="search-movie"
+          className="form-control"
+          aria-describedby="movie-helper"
+          placeholder='Cerca titolo qui'
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+
+        <button type="submit" className="btn btn-primary mt-2" onClick={handleFormSubmit}>
+          Cerca
+        </button>
+      </div>
 
     </>
   )
