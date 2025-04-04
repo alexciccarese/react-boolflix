4c6deaa176f8d7c7ca980e409228e171f3a03215
alexciccarese <alex.ciccarese@gmail.com>
2025-04-04 16:50:57 +0200

    Add search input and button to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router"
+
 export default function App() {
 
-  return(
+
+  return (
 
     <>
-    <h1>Boolflix</h1>
+    <div className="container">
+      <h1>Boolflix</h1>
+
+      
+      <label className="form-label">Cerca per titolo</label>
+      <input
+        type="text"
+        name="search-movie"
+        id="search-movie"
+        className="form-control"
+        aria-describedby="movie-helper"
+        placeholder='Cerca titolo qui'
+      />
+
+      <button 
+      type="submit" 
+      className="btn btn-primary mt-2"
+      >
+        Cerca
+      </button>
+    </div>
+
     </>
   )
 }
