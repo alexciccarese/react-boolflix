d42be96da6d1d98b70af1ce9283acc596f520227
alexciccarese <alex.ciccarese@gmail.com>
2025-04-04 12:22:15 +0200

    Rename MovieContext to GlobalContext and update provider to include setMovie

--- src/context/MovieContext.jsx
+++ src/context/MovieContext.jsx
@@ -1,20 +1,26 @@
 import { createContext, useContext, useState } from "react";
 
-const MovieContext = createContext()
+const GlobalContext = createContext()
 
-const MovieProvider = ({ children }) => {
+const GlobalProvider = ({ children }) => {
 
   const [movie, setMovie] = useState([])
 
-  const api_key = import.meta.env.MOVIE_DB_API_KEY;
-  const base_movies_api_url = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${serchText}`;
+/*   const api_key = import.meta.env.MOVIE_DB_API_KEY;
+  const base_movies_api_url = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${serchText}`; */
 
   return (
 
-    <MovieContext.Provider value={{ movie }}>
+    <MovieContext.Provider value={{ movie, setMovie }}>
       {children}
     </MovieContext.Provider>
   )
 }
 
-export default { MovieContext, MovieProvider }
+function useGlobalContext() {
+  const context = useContext(GlobalContext)
+  return context
+}
+
+
+export default { useGlobalContext, GlobalProvider }
