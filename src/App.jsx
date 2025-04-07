74d207ef27a1c4823312130b2faf823e14ff51a1
alexciccarese <alex.ciccarese@gmail.com>
2025-04-07 19:01:07 +0200

    Pass inputValue to Main component and display search query in the UI

--- src/App.jsx
+++ src/App.jsx
@@ -37,6 +37,7 @@ export default function App() {
       filteredMovies={filteredMovies}
       faStar={faStar}
       faStarEmpty={faStarEmpty}
+      inputValue={inputValue}
       />
 
       <Footer />
